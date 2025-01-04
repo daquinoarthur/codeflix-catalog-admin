c6f999b19400253022a3973e0bf38234b98cd13d
Arthur D'Aquino <deaquinodev@gmail.com>
2025-01-04 18:55:25 -0300

    update test api

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,25 +1,34 @@
-from django.forms.fields import uuid
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import status
+
+from src.core.category.application.use_cases.list_category import (
+    ListCategory,
+    ListCategoryInput,
+)
+from src.django_project.category_app.repository import DjangoORMCategoryRepository
 
 
 # Create your views here.
 class CategoryViewSet(viewsets.ViewSet):
     def list(self, request: Request) -> Response:
+        input = ListCategoryInput()
+        repository = DjangoORMCategoryRepository()
+        use_case = ListCategory(repository)
+
+        output = use_case.execute(input)
+        categories = output.data
+
         return Response(
+            status=status.HTTP_200_OK,
             data=[
                 {
-                    "id": "6b955d67-11a1-4240-8340-c61b9b040bdc",
-                    "name": "Movie",
-                    "description": "Movie description",
-                    "is_active": True,
-                },
-                {
-                    "id": "8b955d67-11a1-4240-8340-c61b9b040bdc",
-                    "name": "Documentary",
-                    "description": "Documentary description",
-                    "is_active": True,
-                },
-            ]
+                    "id": str(category.id),
+                    "name": category.name,
+                    "description": category.description,
+                    "is_active": category.is_active,
+                }
+                for category in categories
+            ],
         )
