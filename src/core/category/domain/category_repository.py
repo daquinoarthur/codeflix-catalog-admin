81df550d2d24b82985ec7ffca7c3b256914c84d9
Arthur D'Aquino <deaquinodev@gmail.com>
2025-01-11 17:34:07 -0300

    Update the types of inputs and outputs for the CategoryRepository abstract class

--- src/core/category/domain/category_repository.py
+++ src/core/category/domain/category_repository.py
@@ -1,23 +1,24 @@
 from abc import ABC, abstractmethod
+from uuid import UUID
 
 from src.core.category.domain.category import Category
 
 
 class CategoryRepository(ABC):
     @abstractmethod
-    def save(self, category) -> Category:
+    def save(self, category: Category) -> Category:
         raise NotImplementedError
 
     @abstractmethod
-    def get_by_id(self, id) -> Category | None:
+    def get_by_id(self, id: UUID) -> Category | None:
         raise NotImplementedError
 
     @abstractmethod
-    def delete(self, id) -> None:
+    def delete(self, id: UUID) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def update(self, category) -> Category:
+    def update(self, category: Category) -> Category:
         raise NotImplementedError
 
     @abstractmethod
