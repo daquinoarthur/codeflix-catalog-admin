a6d2402c96b4750297cc4a89d3206399f255a02f
Arthur D'Aquino <deaquinodev@gmail.com>
2025-01-24 20:35:42 -0300

    test: add list category use case integration test

---
+++ src/core/category/tests/application/integration/test_list_category.py
@@ -0,0 +1,51 @@
+from src.core.category.application.use_cases.list_category import (
+    CategoryOutput,
+    ListCategory,
+    ListCategoryInput,
+    ListCategoryOutput,
+)
+from src.core.category.domain.category import Category
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+
+
+class TestListCategory:
+    def test_return_empty_list(self):
+        repository = InMemoryCategoryRepository()
+        use_case = ListCategory(repository)
+        request = ListCategoryInput()
+        response = use_case.execute(request)
+        if response:
+            assert response == ListCategoryOutput(data=[])
+
+    def test_return_existing_categories(self):
+        category_filme = Category(
+            name="Filme",
+            description="Categoria de filmes",
+        )
+        category_serie = Category(
+            name="Série",
+            description="Categoria de séries",
+        )
+        repository = InMemoryCategoryRepository()
+        repository.save(category_filme)
+        repository.save(category_serie)
+        use_case = ListCategory(repository)
+        request = ListCategoryInput()
+        response = use_case.execute(request)
+        if response:
+            assert response == ListCategoryOutput(
+                data=[
+                    CategoryOutput(
+                        id=category_filme.id,
+                        name=category_filme.name,
+                        description=category_filme.description,
+                        is_active=category_filme.is_active,
+                    ),
+                    CategoryOutput(
+                        id=category_serie.id,
+                        name=category_serie.name,
+                        description=category_serie.description,
+                        is_active=category_serie.is_active,
+                    ),
+                ]
+            )
