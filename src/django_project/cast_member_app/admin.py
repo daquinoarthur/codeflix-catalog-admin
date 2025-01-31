1aa3a85004cfea205d5b358160bf6f31d1283f7a
Arthur D'Aquino <deaquinodev@gmail.com>
2025-01-31 16:01:59 -0300

    feat: add cast member model to the admin panel

--- src/django_project/cast_member_app/admin.py
+++ src/django_project/cast_member_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import CastMember
+
+
 # Register your models here.
+class CastMemberAdmin(admin.ModelAdmin): ...
+
+
+admin.site.register(CastMember, CastMemberAdmin)
