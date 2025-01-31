9d2b6c14e7e3dd25e163b327ceb9e482cbd4213c
Arthur D'Aquino <deaquinodev@gmail.com>
2025-01-31 16:49:55 -0300

    feat: add django orm cast member repository

---
+++ src/django_project/cast_member_app/repository.py
@@ -0,0 +1,69 @@
+from uuid import UUID
+
+from src.core.cast_member.domain.cast_member import CastMember
+from src.core.cast_member.domain.cast_member_repository import CastMemberRepository
+from src.django_project.cast_member_app.models import CastMember as CastMemberModel
+
+
+class DjangoORMCastMemberRepository(CastMemberRepository):
+    def __init__(self, cast_member_model: CastMemberModel | None = None):
+        self.cast_member_model = cast_member_model or CastMemberModel
+
+    def save(self, cast_member: CastMember) -> CastMember:
+        created_cast_member = self.cast_member_model.objects.create(
+            id=cast_member.id,
+            name=cast_member.name,
+            type=cast_member.type,
+        )
+        return CastMember(
+            id=created_cast_member.id,
+            name=created_cast_member.name,
+            type=created_cast_member.type,
+        )
+
+    def get_by_id(self, id: UUID) -> CastMember | None:
+        try:
+            cast_member = self.cast_member_model.objects.get(id=id)
+        except self.cast_member_model.DoesNotExist:
+            return None
+        return CastMember(
+            id=cast_member.id,
+            name=cast_member.name,
+            type=cast_member.type,
+        )
+
+    def delete(self, id: UUID) -> None:
+        self.cast_member_model.objects.filter(id=id).delete()
+
+    def list(self) -> list[CastMember]:
+        return [
+            CastMember(
+                id=cast_member.id,
+                name=cast_member.name,
+                type=cast_member.type,
+            )
+            for cast_member in self.cast_member_model.objects.all()
+        ]
+
+    def update(self, cast_member: CastMember) -> CastMember | None:
+        try:
+            persisted_cast_member = (
+                self.cast_member_model.objects.select_for_update().get(
+                    id=cast_member.id
+                )
+            )
+            update_fields = {
+                "name": cast_member.name,
+                "type": cast_member.type,
+            }
+            self.cast_member_model.objects.filter(id=cast_member.id).update(
+                **update_fields
+            )
+            persisted_cast_member.refresh_from_db()
+            return CastMember(
+                id=persisted_cast_member.id,
+                name=persisted_cast_member.name,
+                type=persisted_cast_member.type,
+            )
+        except self.cast_member_model.DoesNotExist:
+            return None
