33212f1c6d261752f2588cae4a733fd3909b9c9c
Arthur D'Aquino <deaquinodev@gmail.com>
2025-02-02 20:25:03 -0300

    feat: add ListCastMemberResponseSerializer for list cast member

---
+++ src/django_project/cast_member_app/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from src.core.cast_member.domain.cast_member import CastMemberType
+
+
+class CastMemberTypeField(serializers.ChoiceField):
+    def __init__(self, **kwargs):
+        choices = [
+            (cast_member_type.value, cast_member_type.value)
+            for cast_member_type in CastMemberType
+        ]
+        super().__init__(choices=choices, **kwargs)
+
+    def to_internal_value(self, data):
+        return CastMemberType(super().to_internal_value(data))
+
+    def to_representation(self, value):
+        return str(super().to_representation(value))
+
+
+class CastMemberSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    name = serializers.CharField()
+    type = CastMemberTypeField()
+
+
+class ListCastMemberResponseSerializer(serializers.Serializer):
+    data = CastMemberSerializer(many=True)
