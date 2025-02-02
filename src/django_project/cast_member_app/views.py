cd404c6cf4b54bdca8a944e22c299961e5e2e461
Arthur D'Aquino <deaquinodev@gmail.com>
2025-02-02 20:24:32 -0300

    feat: add list method for CastMemberViewSet

--- src/django_project/cast_member_app/views.py
+++ src/django_project/cast_member_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.views import Request, Response
+
+from src.django_project.cast_member_app.repository import DjangoORMCastMemberRepository
+from src.core.cast_member.application.use_cases.list_cast_members import (
+    ListCastMember,
+)
+from src.django_project.cast_member_app.serializers import (
+    ListCastMemberResponseSerializer,
+)
+
 
 # Create your views here.
+class CastMemberViewSet(viewsets.ViewSet):
+    def list(self, request: Request) -> Response:
+        repository = DjangoORMCastMemberRepository()
+        use_case = ListCastMember(repository=repository)
+        input = ListCastMember.Input()
+        output = use_case.execute(input)
+        response_serializer = ListCastMemberResponseSerializer(output)
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
