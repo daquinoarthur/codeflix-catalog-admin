14c2f00fb7d9ce4c58bd69cbf7f38a8c18aabc23
Arthur D'Aquino <deaquinodev@gmail.com>
2025-02-02 23:25:33 -0300

    test: add create cast member viewset test

--- src/django_project/cast_member_app/tests/test_views.py
+++ src/django_project/cast_member_app/tests/test_views.py
@@ -1,6 +1,6 @@
 import pytest
 from rest_framework.test import APIClient
-
+from rest_framework.fields import ErrorDetail
 from src.core.cast_member.domain.cast_member import CastMember, CastMemberType
 from src.core.cast_member.domain.cast_member_repository import CastMemberRepository
 from src.django_project.cast_member_app.repository import DjangoORMCastMemberRepository
@@ -23,10 +23,9 @@ class CommonTestFixtures:
         )
 
 
-class TestListCastMembersView(CommonTestFixtures):
+class TestCastMemberViewSetListAPI(CommonTestFixtures):
     def test_list_cast_members(
         self,
-        cast_member_repository,
         create_cast_member,
         client,
     ):
@@ -59,3 +58,47 @@ class TestListCastMembersView(CommonTestFixtures):
         assert response.data == expected_response
         assert response.status_code == 200
         assert len(response.data["data"]) == 0
+
+
+class TestCastMemberViewSetCreateAPI(CommonTestFixtures):
+    def test_create_cast_member(self, cast_member_repository, client):
+        cast_member_path = "/api/cast-members/"
+        payload = {
+            "name": "Actor",
+            "type": "ACTOR",
+        }
+        response = client.post(cast_member_path, payload, format="json")
+        created_cast_member_actor = cast_member_repository.list()[0]
+        expected_response = {
+            "id": str(created_cast_member_actor.id),
+            "name": created_cast_member_actor.name,
+            "type": created_cast_member_actor.type.value,
+        }
+        assert len(cast_member_repository.list()) == 1
+        assert response.data == expected_response
+
+    def test_create_cast_member_with_invalid_name_field(self, client):
+        cast_member_path = "/api/cast-members/"
+        payload = {
+            "name": "",
+            "type": "ACTOR",
+        }
+        response = client.post(cast_member_path, payload, format="json")
+        assert response.data == {
+            "name": [ErrorDetail(string="This field may not be blank.", code="blank")]
+        }
+
+    def test_create_cast_member_with_invalid_type_field(self, client):
+        cast_member_path = "/api/cast-members/"
+        payload = {
+            "name": "Actor",
+            "type": "INVALID",
+        }
+        response = client.post(cast_member_path, payload, format="json")
+        assert response.data == {
+            "type": [
+                ErrorDetail(
+                    string='"INVALID" is not a valid choice.', code="invalid_choice"
+                )
+            ]
+        }
