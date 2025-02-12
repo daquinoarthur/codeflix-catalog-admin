1bd9b50ea8b6266a3b2c78a982360bfb2cd1eb6f
Arthur D'Aquino <deaquinodev@gmail.com>
2025-02-12 17:55:13 -0300

    chore: fix 'id' field on CastMember model

--- src/django_project/cast_member_app/models.py
+++ src/django_project/cast_member_app/models.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from django.db import models
 
 from src.core.cast_member.domain.cast_member import CastMemberType
@@ -6,7 +7,7 @@ from src.core.cast_member.domain.cast_member import CastMemberType
 # Create your models here.
 class CastMember(models.Model):
     app_label = "cast_member_app"
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid4)
     name = models.CharField(max_length=255)
     type = models.CharField(
         max_length=255,
